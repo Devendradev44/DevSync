75793a8cb80624b464de288cc6a25e7f077f6099
devendra <deva131635@gmail.com>
2026-06-19 19:27:23 +0530

    Initial commit: Microsoft Loop clone DevSync

---
+++ middleware.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isProtectedRoute = createRouteMatcher([
+  "/dashboard(.*)",
+]);
+
+export default clerkMiddleware(async (auth, req) => {
+  if (isProtectedRoute(req)) {
+    const { userId } = await auth();
+
+    if (!userId) {
+      return NextResponse.redirect(new URL("/sign-in", req.url));
+    }
+  }
+});
+
+export const config = {
+  matcher: [
+    "/((?!_next|.*\\..*).*)",
+    "/(api|trpc)(.*)",
+  ],
+};
