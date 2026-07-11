1d94405230fec7bda018b08297f0923f0238b618
devendra <deva131635@gmail.com>
2026-07-12 01:45:45 +0530

    workspace updates

--- app/(routes)/createworkspace/page.jsx
+++ app/(routes)/createworkspace/page.jsx
@@ -1,21 +1,41 @@
 "use client"
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { SmilePlus } from 'lucide-react';
 import Image from 'next/image'
 import React from 'react'
 import { useState } from 'react'
 
 function CreateWorkspace() {
   const [coverImage, setCoverImage] = useState('/cover.png');
+  const [workspaceName,setWorkspaceName] = useState();
+
   return (
-    <div className="p-10 md:px-36 lg:px-52 xl:px-80 py-20">
-      <div>
-        
-        <div className="relative group">
+    <div className="p-10 md:px-36 lg:px-64 xl:px-96 py-28">
+      <div className="shadow-2xl rounded-xl">
+        {/* Cover Image */}
+        <div className="relative group cursor-pointer">
           <h2 className="hidden absolute p-4 w-full h-full items-center justify-center
           group-hover:flex ">Change Cover</h2>
-          <div>
-          <Image src={coverImage} alt="Cover Image" width={400} height={400} 
+          <div className="group-hover:opacity-40">
+            <Image src={coverImage} alt="Cover Image" width={400} height={400} 
             className="w-full h-[150px] object-cover rounded-t-xl" />
+          </div>
+        </div>
+        {/* Input Section */}
+        <div className="p-12">
+          <h2 className="font-medium text-xl">Create a new workspace</h2>
+          <h2 className="text-sm mt-2">This is a shared space where you can collaborate with your team.</h2>
 
+          <div className="mt-8 flex gap-2 items-center">
+            <Button variant="outline">
+              <SmilePlus />
+            </Button>
+            <Input placeholder="Workspace Name" onChange={(e) =>setWorkspaceName(e.target.value)} />
+          </div>
+          <div className="mt-7 flex justify-end">
+            <Button disabled={!workspaceName?.length}>Create</Button>
+            <Button variant="outline">Cancel</Button>
           </div>
         </div>
       </div>
