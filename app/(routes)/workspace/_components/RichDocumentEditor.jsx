70ee98689af5fbccccdaa5227f90fd1e1fe2f8da
devendra <deva131635@gmail.com>
2026-07-20 00:39:48 +0530

    document editor js packages installed

--- app/(routes)/workspace/_components/RichDocumentEditor.jsx
+++ app/(routes)/workspace/_components/RichDocumentEditor.jsx
@@ -1,29 +1,110 @@
-import React, { useEffect, useRef } from 'react'
-import EditorJS from '@editorjs/editorjs';
+"use client";
+
+import React, { useEffect, useRef, useState } from "react";
+import EditorJS from "@editorjs/editorjs";
+import Header from "@editorjs/header";
+import Delimiter from "@editorjs/delimiter";
+import Alert from "editorjs-alert";
+import List from "@editorjs/list";
+import NestedList from '@editorjs/nested-list';
+import Checklist from '@editorjs/checklist'
+import Embed from '@editorjs/embed';
+import SimpleImage from 'simple-image-editorjs';
+import Table from '@editorjs/table'
+import CodeTool from '@editorjs/code';
+import { TextVariantTune } from '@editorjs/text-variant-tune';
+import { doc, onSnapshot, updateDoc } from 'firebase/firestore';
+import { db } from '@/config/firebaseConfig';
+import { useUser } from '@clerk/nextjs';
+import Paragraph from '@editorjs/paragraph';
+import GenerateAITemplate from './GenerateAITemplate';
 
 function RichDocumentEditor() {
+  const editorRef = useRef(null);
+  const [documentOutput, setDocumentOutput] = useState([]);
+  // const [isFetched,setIsFetched]=useState(false);
 
-    const ref = useRef();
-    let editor;
+  let isFetched=false
+    const SaveDocument = () => {
+    console.log("UPDATE")
+    ref.current.save().then(async (outputData) => {
+      const docRef = doc(db, 'documentOutput', params?.documentid);
+     
+      await updateDoc(docRef, {
+        output: JSON.stringify(outputData),
+        editedBy: user?.primaryEmailAddress?.emailAddress
+      })
+    })
+  }
 
-    useEffect(()=>{
-        InitEditor();
-    },[]);
-
-    const InitEditor=()=>
-    {
-        if(!editor?.current){
-            const editor = new EditorJS({
-                holder:'editorjs'
-            });
-            ref.current=editor
-        }
+  const GetDocumentOutput = () => {
+    const unsubscribe = onSnapshot(doc(db, 'documentOutput', params?.documentid),
+      (doc) => {
+        if (doc.data()?.editedBy != user?.primaryEmailAddress?.emailAddress||isFetched==false)
+          doc.data().editedBy&&editor?.render(JSON.parse(doc.data()?.output)); 
+        isFetched=true  
+      })
+  }
+  
+  useEffect(() => {
+    if (!editorRef.current) {
+      editorRef.current = new EditorJS({
+        onChange:(ap,event)=>{
+            SaveDocument()
+        },
+        onReady:()=>{
+            GetDocumentOutput()
+        },
+        holder: "editorjs",
+        tools: {
+          header: Header,
+          delimiter: Delimiter,
+          alert: {
+            class: Alert,
+            inlineToolbar: true,
+            shortcut: 'CMD+SHIFT+A',
+            config: {
+                alertTypes: ['primary', 'secondary', 'info', 'success', 'warning', 'danger', 'light', 'dark'],
+                defaultType: 'primary',
+                messagePlaceholder: 'Enter something',
+            },
+            table: Table,
+          list: {
+            class: List,
+            inlineToolbar: true,
+            shortcut: 'CMD+SHIFT+L',
+            config: {
+              defaultStyle: 'unordered'
+            },
+          },
+          checklist: {
+            class: Checklist,
+            shortcut: 'CMD+SHIFT+C',
+            inlineToolbar: true,
+          },
+          image: SimpleImage,
+          code: {
+            class: CodeTool,
+            shortcut: 'CMD+SHIFT+P'
+          },
+            },
+        },
+      });
     }
+
+    return () => {
+      if (editorRef.current?.destroy) {
+        editorRef.current.destroy();
+        editorRef.current = null;
+      }
+    };
+  }, []);
+
   return (
-    <div className="px-20 ml-10">
+    <div className="-ml-40">
       <div id="editorjs"></div>
     </div>
-  )
+  );
 }
 
-export default RichDocumentEditor
+export default RichDocumentEditor;
